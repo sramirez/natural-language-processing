9e1252c6e573e7a568de24562019557dfbd57794
Sergio R <srg00017@gmail.com>
2019-06-22 12:54:47 +0200

    Added last version python files, and pickles

--- project/utils.py
+++ project/utils.py
@@ -18,7 +18,7 @@ RESOURCE_PATH = {
 
 def text_prepare(text):
     """Performs tokenization and simple preprocessing."""
-    
+
     replace_by_space_re = re.compile('[/(){}\[\]\|@,;]')
     bad_symbols_re = re.compile('[^0-9a-z #+_]')
     stopwords_set = set(stopwords.words('english'))
@@ -41,36 +41,34 @@ def load_embeddings(embeddings_path):
       embeddings - dict mapping words to vectors;
       embeddings_dim - dimension of the vectors.
     """
-    
+
     # Hint: you have already implemented a similar routine in the 3rd assignment.
     # Note that here you also need to know the dimension of the loaded embeddings.
     # When you load the embeddings, use numpy.float32 type as dtype
 
-    ########################
-    #### YOUR CODE HERE ####
-    ########################
-
-    # remove this when you're done
-    raise NotImplementedError(
-        "Open utils.py and fill with your code. In case of Google Colab, download"
-        "(https://github.com/hse-aml/natural-language-processing/blob/master/project/utils.py), "
-        "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
+    embeddings = {}
+    embeddings_dim = 0
+    for line in open(embeddings_path):
+        q, *emb  = line.strip().split('\t')
+        embeddings[q] = [float(e) for e in emb]
+        embeddings_dim = len(embeddings[q])
+    return (embeddings, embeddings_dim)
 
 
-def question_to_vec(question, embeddings, dim):
+def question_to_vec(question, embeddings, dim=300):
     """Transforms a string to an embedding by averaging word embeddings."""
-    
+
     # Hint: you have already implemented exactly this function in the 3rd assignment.
 
-    ########################
-    #### YOUR CODE HERE ####
-    ########################
+    words = question.split()
+    result = np.zeros(dim)
 
-    # remove this when you're done
-    raise NotImplementedError(
-        "Open utils.py and fill with your code. In case of Google Colab, download"
-        "(https://github.com/hse-aml/natural-language-processing/blob/master/project/utils.py), "
-        "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
+    if words:
+        vectors = [embeddings[w] for w in words if w in embeddings]
+    if vectors:
+        result = np.sum(vectors, axis=0) / len(vectors)
+
+    return result
 
 
 def unpickle_file(filename):
